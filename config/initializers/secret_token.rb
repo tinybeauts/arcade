9457db8df333d9d7aa61ee75204631ca28016cdb
Julianne Beaudoin <jubeaudoin@gmail.com>
2013-05-14 17:22:37 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arcade::Application.config.secret_token = 'dd52f11a78f7eaa1af2f278036411560986dceb2102117308d272de11fd97f7bf03d4af171eadad2619d5afec014646cedafc62144063d60279c08a7b3748b7c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Arcade::Application.config.session_store :cookie_store, key: '_arcade_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Arcade::Application.config.session_store :active_record_store
